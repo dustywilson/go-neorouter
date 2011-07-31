5522057987a80cd13d28ced950547a43f5280c81
Dusty Wilson <dusty@megagram.com>
2011-07-30 23:09:11 -0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,6 @@
+include $(GOROOT)/src/Make.inc
+
+TARG=neorouter
+GOFILES=neorouter.go
+
+include $(GOROOT)/src/Make.pkg
---
+++ neorouter.go
@@ -0,0 +1,68 @@
+package neorouter
+
+
+import (
+	"fmt"
+	"exec"
+	"bufio"
+	"regexp"
+)
+
+
+type List struct {
+	Computers []Computer
+}
+
+type Computer struct {
+	Group    string
+	Name     string
+	Ip       string
+	IsOnline bool
+}
+
+
+func GetList(domain, username, password string) (list List) {
+	cmd := exec.Command("/usr/bin/nrclientcmd", "-p", password)
+	cmdStdin, _ := cmd.StdinPipe()   // in  = into process from me
+	cmdStdout, _ := cmd.StdoutPipe() // out = out from process to me
+	reader := bufio.NewReader(cmdStdout)
+	reDomain := regexp.MustCompile("Domain:")
+	reUsername := regexp.MustCompile("Username:")
+	reGroupname := regexp.MustCompile("> +([0-9A-Za-z]+)")
+	reComputerOffline := regexp.MustCompile("\\(offline\\) +([0-9A-Za-z ]+)")
+	reComputerOnline := regexp.MustCompile("([0-9\\.]+) +([0-9A-Za-z ]+)")
+	cmd.Start()
+	for {
+		line, _ := reader.ReadString(':')
+		if reDomain.Match([]byte(line)) {
+			cmdStdin.Write([]byte(fmt.Sprintf("%s\n", domain)))
+		} else if reUsername.Match([]byte(line)) {
+			cmdStdin.Write([]byte(fmt.Sprintf("%s\n", username)))
+			break
+		} else {
+			break
+		}
+	}
+	groupname := "UNASSIGNED"
+	for {
+		line, _, err := reader.ReadLine()
+		if err != nil { // probably EOF which is okay
+			break
+		}
+		if match := reGroupname.FindStringSubmatch(string(line)); match != nil {
+			groupname = match[1]
+		} else if match := reComputerOffline.FindStringSubmatch(string(line)); match != nil {
+			computername := match[1]
+			computer := Computer{Group: groupname, Name: computername, IsOnline: false}
+			list.Computers = append(list.Computers, computer)
+		} else if match := reComputerOnline.FindStringSubmatch(string(line)); match != nil {
+			ip := match[1]
+			computername := match[2]
+			computer := Computer{Group: groupname, Name: computername, Ip: ip, IsOnline: true}
+			list.Computers = append(list.Computers, computer)
+		}
+		cmdStdin.Write([]byte("quit\n"))
+	}
+	cmd.Process.Kill()
+	return list
+}
